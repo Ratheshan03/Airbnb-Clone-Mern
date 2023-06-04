794690af67e85abcc30b8434264916570d7a9bf5
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2023-06-04 22:07:32 +0530

    register completed and login ongoing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = require("./models/User.js");
+require("dotenv").config();
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
 
@@ -11,13 +19,52 @@ app.use(
   })
 );
 
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URL);
+
+// Routes
 app.get("/test", (req, res) => {
   res.json("Testing Done");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({ name, email, password });
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+
+    res.json(userDoc);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+      const isPasswordValid = bcrypt.compareSync(password, userDoc.password);
+      if (isPasswordValid) {
+        jwt.sign(
+          { email: userDoc.email, id: userDoc._id },
+          jwtSecret,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res.cookie("token", token).json("Password is valid");
+          }
+        );
+      } else {
+        res.status(422).json("Password is invalid");
+      }
+    }
+  } catch (error) {
+    res.json("User not found");
+  }
 });
 
 app.listen(3001);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,20 @@ export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function registerUser(ev) {
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.post("/regsiter", {
-      name,
-      email,
-      password,
-    });
+
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration Successfull!. Now you can Login");
+    } catch (err) {
+      alert("Registration failed!. Please try again");
+    }
   }
 
   return (
