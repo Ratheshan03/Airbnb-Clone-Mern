d87ebfd16ab922d24600ad346bab17c525ae0a7f
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2023-06-11 23:00:48 +0530

    code-refactor

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,167 +1,32 @@
-import { useEffect, useState } from "react";
-import { Link, useParams, Navigate } from "react-router-dom";
-import axios from "axios";
-import Perks from "../Perks";
-import PhotosUploader from "../PhotosUploader";
+import { Link } from "react-router-dom";
+import AccountNav from "../AccountNav";
 
 export default function PlacesPage() {
-  const { action } = useParams();
-  const [title, setTitle] = useState("");
-  const [address, setAddress] = useState("");
-  const [addedPhotos, setAddedPhotos] = useState([]);
-  const [description, setDescription] = useState("");
-  const [perks, setPerks] = useState([]);
-  const [extraInfo, setExtraInfo] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [maxGuests, setMaxGuests] = useState(1);
-  const [redirect, setRedirect] = useState("");
-
-  function inputHeader(text) {
-    return <h2 className="text-2xl mt-4">{text}</h2>;
-  }
-
-  function inputDescription(text) {
-    return <p className="text-gray-500 text-sm">{text}</p>;
-  }
-
-  function preInput(header, description) {
-    return (
-      <>
-        {inputHeader(header)}
-        {inputDescription(description)}
-      </>
-    );
-  }
-
-  async function addNewPlace(ev) {
-    ev.preventDefault();
-    const { data } = await axios.post("/places", {
-      title,
-      address,
-      addedPhotos,
-      description,
-      perks,
-      extraInfo,
-      checkIn,
-      checkOut,
-      maxGuests,
-    });
-
-    setRedirect("/account/places");
-  }
-
-  if (redirect) {
-    return <Navigate to={redirect} />;
-  }
-
   return (
     <div>
-      {action !== "new" && (
-        <div className="text-center">
-          <Link
-            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-            to={"/account/places/new"}
+      <AccountNav />
+      <div className="text-center">
+        List of all added Places
+        <br />
+        <Link
+          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-                clipRule="evenodd"
-              />
-            </svg>
-            Add new place
-          </Link>
-        </div>
-      )}
-
-      {action === "new" && (
-        <div>
-          <form onSubmit={addNewPlace}>
-            {preInput(
-              "Title",
-              "title for your place. Should be short and catchy as in advertisement"
-            )}
-            <input
-              type="text"
-              placeholder="title, for ex: My Lovely Apartment"
-              value={title}
-              onChange={(ev) => setTitle(ev.target.value)}
+            <path
+              fillRule="evenodd"
+              d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+              clipRule="evenodd"
             />
-
-            {preInput("Address", "Address to this place")}
-            <input
-              type="text"
-              placeholder="address"
-              value={address}
-              onChange={(ev) => setAddress(ev.target.value)}
-            />
-
-            {preInput("Photos", "more = better")}
-            <PhotosUploader
-              addedPhotos={addedPhotos}
-              onChange={setAddedPhotos}
-            />
-
-            {preInput("Description", "Description of the place")}
-            <textarea
-              value={description}
-              onChange={(ev) => setDescription(ev.target.value)}
-            />
-
-            {preInput("Perks", "Select all the perks of the place")}
-            <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              <Perks selected={perks} onChange={setPerks} />
-            </div>
-
-            {preInput("Extra Info", "House Rules, etc")}
-            <textarea
-              value={extraInfo}
-              onChange={(ev) => setExtraInfo(ev.target.value)}
-            />
-
-            {preInput(
-              "Check in&out times, max guests",
-              "add check in and out imes, remember to include the max number of guests can be occupied in the property"
-            )}
-            <div className="grid gap-2 sm:grid-cols-3">
-              <div>
-                <h3 className="mt-2 -mb-1 ">Check in time</h3>
-                <input
-                  type="text"
-                  placeholder="14:00"
-                  value={checkIn}
-                  onChange={(ev) => setCheckIn(ev.target.value)}
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1 ">Check out time</h3>
-                <input
-                  type="text"
-                  value={checkOut}
-                  onChange={(ev) => setCheckOut(ev.target.value)}
-                  placeholder="03:00"
-                />
-              </div>
-              <div>
-                <h3 className="mt-2 -mb-1 ">Max number of guests</h3>
-                <input
-                  type="number"
-                  value={maxGuests}
-                  onChange={(ev) => setMaxGuests(ev.target.value)}
-                />
-              </div>
-            </div>
-
-            <button className="primary my-4">Save</button>
-          </form>
-        </div>
-      )}
+          </svg>
+          Add new place
+        </Link>
+      </div>
     </div>
   );
 }
